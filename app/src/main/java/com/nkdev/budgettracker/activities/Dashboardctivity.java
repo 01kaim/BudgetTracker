5f4fef1701e216cdd2bff3ad6800db90a372d98d
01kaim <kamal.kaim.naim@gmail.com>
2023-06-20 20:47:56 +0100

    new edit

--- app/src/main/java/com/nkdev/budgettracker/activities/Dashboardctivity.java
+++ app/src/main/java/com/nkdev/budgettracker/activities/Dashboardctivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.nkdev.budgettracker.R;
 import com.nkdev.budgettracker.adapter.ItemAdapter;
@@ -12,10 +15,11 @@ import com.nkdev.budgettracker.adapter.ItemAdapter;
 import java.util.ArrayList;
 
  public class Dashboardctivity extends AppCompatActivity {
-     RecyclerView recyclerView1;
+     RecyclerView recyclerView1,recyclerView2;
      LinearLayoutManager layoutManager;
      ItemAdapter itemAdapter;
      ArrayList<String> datasource;
+     ImageButton BtnMore;
      int [] IdImage={R.drawable.ic_bookmark,R.drawable.ic_reminder,R.drawable.ic_budget};
 
     @Override
@@ -23,6 +27,14 @@ import java.util.ArrayList;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboardctivity);
         recyclerView1=findViewById(R.id.id_horizontal_RV);
+        recyclerView2=findViewById(R.id.id_vertical_RV);
+        BtnMore=findViewById(R.id.id_btn_more);
+        BtnMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( Dashboardctivity.this,EntriesActivity.class));
+            }
+        });
         datasource=new ArrayList<String>();
         datasource.add("Savings");
         datasource.add("Reminders");
